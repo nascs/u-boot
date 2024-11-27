009578ffc655b48276dab69eacc5273a55c09ba1
Nascs Fang <nascs@radxa.com>
2024-11-27 19:00:37 +0800

    common: add ANDROID_ARG_SLOT_SUFFIX conditional compilation option
    Signed-off-by: Nascs Fang <nascs@radxa.com>

--- common/spl/spl_ab.c
+++ common/spl/spl_ab.c
@@ -416,12 +416,15 @@ int spl_ab_decrease_reset(struct blk_desc *dev_desc)
 
 int spl_ab_bootargs_append_slot(void *fdt, char *slot)
 {
-	char *str;
-	int len, ret = 0;
+
+	int ret = 0;
 
 	if (!slot)
 		return 0;
 
+#ifdef ANDROID_ARG_SLOT_SUFFIX
+	int len;
+	char *str;
 	len = strlen(ANDROID_ARG_SLOT_SUFFIX) + strlen(slot) + 1;
 	str = malloc(len);
 	if (!str)
@@ -433,6 +436,9 @@ int spl_ab_bootargs_append_slot(void *fdt, char *slot)
 		printf("Append slot info to bootargs fail");
 
 	free(str);
+#else
+	printf("ANDROID_ARG_SLOT_SUFFIX not defined. Skipping slot append.\n");
+#endif
 
 	return ret;
 }
